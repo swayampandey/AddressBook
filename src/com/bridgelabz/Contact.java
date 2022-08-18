8ac408f743140fd62efbaae0b161024b667dd18e
swayampandey <swayam265@gmail.com>
2022-08-18 22:22:41 +0530

    [Swayam] : added Ability to add new contact in Address Book functionality

--- src/com/bridgelabz/Contact.java
+++ src/com/bridgelabz/Contact.java
@@ -1,43 +1,59 @@
 package com.bridgelabz;
-import java.util.Scanner;
+
 
 public class Contact {
 	String First, Last, Address,  City, State;
 	long PhoneNum, Zip;
-	
-	static Scanner sc = new Scanner(System.in);
-	
-	public void contactDetails() {
-		System.out.println("Enter first name : ");
-		First = sc.nextLine();
-		System.out.println("Enter last name : ");
-		Last = sc.nextLine();
-		System.out.println("Enter address : ");
-		Address = sc.nextLine();
-		System.out.println("Enter city name : ");
-		City = sc.nextLine();
-		System.out.println("Enter state name : ");
-		State = sc.next();
-		System.out.println("Enter mobile number : ");
-		PhoneNum = sc.nextLong();
-		System.out.println("Enter zip code : ");
-		Zip = sc.nextLong();
+	public String getFirst() {
+		return First;
 	}
-	public void display() {
-		System.out.println("Enter first name    : " +First);
-		
-		System.out.println("Enter last name     : " +Last);
-		
-		System.out.println("Enter address       : " +Address);
-		
-		System.out.println("Enter city name     : " +City);
-		
-		System.out.println("Enter state name    : " +State);
-		
-		System.out.println("Enter mobile number : " +PhoneNum);
-		
-		System.out.println("Enter zip code      : " +Zip);
-		
+	public void setFirst(String first) {
+		First = first;
 	}
+	public String getLast() {
+		return Last;
+	}
+	public void setLast(String last) {
+		Last = last;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public long getPhoneNum() {
+		return PhoneNum;
+	}
+	public void setPhoneNum(long phoneNum) {
+		PhoneNum = phoneNum;
+	}
+	public long getZip() {
+		return Zip;
+	}
+	public void setZip(long zip) {
+		Zip = zip;
+	}
+	@Override
+	public String toString() {
+		return "\n PERSON DETAILS \n firstName = " + First + " \n lastName = " + Last 
+				+ "\n address = " + Address + "\n city = "
+				+ City + "\n state = " + State + "\n zip Code = " + Zip 
+				+ "\n phoneNo = " + PhoneNum+"\n-Successfully added "+First+" into new contacts\n";
+	}
+	
+	
 
 }
